b94a0a5f5f1af7407406c27aeacc206331cf8641
Louis Lim <curtsky@outlook.com>
2022-03-25 09:40:51 +0100

    Complete 19x16 div and next mapping it

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,15 +1,26 @@
 "use strict";
  
-var divGameArea = document.createElement("div");
-divGameArea.innerHTML = "Div";
-let space = "";
+const divGameArea = document.createElement("div");
 //Appending Div 19x16
 
 for (let colInside = 0; colInside<19; colInside++){
-    for (let rowInside = 0; rowInside<16; rowInside)
+    for (let rowInside = 0; rowInside<16; rowInside++)
     {
-        document.getElementById("myID").appendChild(divGameArea);
-        document.getElementByTagName("Div").appendChild("ID = "+"R"+rowInside+"C"+colInside);
-        console.log(divGameArea);
+        document.getElementById("myGame").appendChild(divGameArea);
+        divGameArea.setAttribute("id", "Y"+colInside+"X"+rowInside);
+        divGameArea.id = "Y"+colInside+"X"+rowInside;
+        console.log(divGameArea.id);
     }
 }
+/*
+// Test page to create 10 Div
+const forDiv = document.createElement("div");
+
+// Add ID to DIV element:
+for (let count=0; count<10; count++){
+        document.getElementById("myDIV").appendChild(forDiv);
+        forDiv.setAttribute('id', count);
+        forDiv.id = "R"+count;    
+        console.log(forDiv.id);
+}
+*/

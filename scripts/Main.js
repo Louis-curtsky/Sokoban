384b55151112e47f107d00e9b96137c5e51e10a8
Louis Lim <curtsky@outlook.com>
2022-03-26 05:41:23 +0100

    Raw map is drawn

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,18 +1,78 @@
 "use strict";
  
-const divGameArea = document.createElement("div");
+//
 //Appending Div 19x16
 
-for (let colInside = 0; colInside<19; colInside++){
-    for (let rowInside = 0; rowInside<16; rowInside++)
+var divGameArea = document.createElement("div");
+var contentFromMap = tileMap01.mapGrid;
+
+for (let colInside = 0; colInside<16; colInside++)
+{
+    for (let rowInside = 0; rowInside<19; rowInside++)
     {
+        if (colInside==0 && rowInside ==0)
+        {
         document.getElementById("myGame").appendChild(divGameArea);
         divGameArea.setAttribute("id", "Y"+colInside+"X"+rowInside);
         divGameArea.id = "Y"+colInside+"X"+rowInside;
-        console.log(divGameArea.id);
+        } else
+        {
+            divGameArea = document.createElement("div");
+            document.getElementById("myGame").appendChild(divGameArea);
+            divGameArea.setAttribute("id", "Y"+colInside+"X"+rowInside);
+            divGameArea.id = "Y"+colInside+"X"+rowInside;
+        }
+    document.getElementById("Y"+colInside+"X"+rowInside).innerHTML = contentFromMap[colInside][rowInside][0];
+//    console.log(document.getElementById("Y"+colInside+"X"+rowInside));
     }
 }
-/*
+
+const userPosition = Y11X11;
+
+document.getElementById("sokoBanBody").onkeydown = function() {keyIsPress()};
+document.getElementById("sokoBanBody").onkeyup = function() {keyIsRelease()};
+
+function keyIsPress() {
+
+  document.getElementById("msgArea").innerHTML="Key is Press";
+}
+    
+function keyIsRelease() {
+    document.getElementById("msgArea").innerHTML="Key is Released";
+  }
+
+  /* 
+  This is to find out P-User position. However this may not neccessary because
+  assuming a fix position for user is always begin at Y11X11
+  */
+
+  /*
+  for (let colInside = 0; colInside<16; colInside++)
+    {
+      for (let rowInside = 0; rowInside<19; rowInside++)
+      {
+          if (document.getElementById("Y"+colInside+"X"+rowInside).innerHTML === "P")
+          {
+            console.log("Poition: Y"+colInside+"X"+rowInside);
+            var posIs = "Y"+colInside+"X"+rowInside;
+          }
+      }
+    }
+    console.log(document.getElementById(posIs).innerHTML);
+  */
+  /*    
+  // Get all the span elements and loop through them
+  divGameArea.querySelectorAll("div").forEach(function(element){
+    // Check the textContent of the element for a match
+    if(element.textContent === "P"){
+      // Find the nearest ancestor div
+      let closest = element.closest("div")
+      // ...then do whatever you want with it
+      console.log("The " + closest.nodeName + " has an id of: " + closest.id);
+    }
+  });
+
+
 // Test page to create 10 Div
 const forDiv = document.createElement("div");
 

426a504d2c345db6a200f13e4d639f0ec427421d
Louis Lim <curtsky@outlook.com>
2022-03-31 08:49:33 +0200

    Play move and wall stop

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,52 +1,288 @@
 "use strict";
- 
-//
-//Appending Div 19x16
+
+// Initailization
 
 var divGameArea = document.createElement("div");
 var contentFromMap = tileMap01.mapGrid;
+var innerArea = document.getElementById("myGame");
+const str = "";
+var gameStart = false;
+var gameOver = true;
 
-for (let colInside = 0; colInside<16; colInside++)
+function msgInfo(str)
 {
-    for (let rowInside = 0; rowInside<19; rowInside++)
-    {
-        if (colInside==0 && rowInside ==0)
-        {
-        document.getElementById("myGame").appendChild(divGameArea);
-        divGameArea.setAttribute("id", "Y"+colInside+"X"+rowInside);
-        divGameArea.id = "Y"+colInside+"X"+rowInside;
-        } else
-        {
-            divGameArea = document.createElement("div");
-            document.getElementById("myGame").appendChild(divGameArea);
-            divGameArea.setAttribute("id", "Y"+colInside+"X"+rowInside);
-            divGameArea.id = "Y"+colInside+"X"+rowInside;
-        }
-    document.getElementById("Y"+colInside+"X"+rowInside).innerHTML = contentFromMap[colInside][rowInside][0];
-//    console.log(document.getElementById("Y"+colInside+"X"+rowInside));
-    }
+  document.getElementById("msgArea").innerHTML = str;
 }
 
-const userPosition = Y11X11;
-
-document.getElementById("sokoBanBody").onkeydown = function() {keyIsPress()};
-document.getElementById("sokoBanBody").onkeyup = function() {keyIsRelease()};
-
-function keyIsPress() {
-
-  document.getElementById("msgArea").innerHTML="Key is Press";
-}
-    
-function keyIsRelease() {
-    document.getElementById("msgArea").innerHTML="Key is Released";
+//Appending Div 19x16
+function startGame()
+{
+  if (gameStart == false)
+  {
+    msgInfo("Start of Game!!!");
   }
+  else
+  {
+  }
+  for (let colInside = 0; colInside<16; colInside++)
+  {
+    for (let rowInside = 0; rowInside<19; rowInside++)
+    {   
+      
+      divGameArea = document.createElement("div");
+      innerArea.appendChild(document.createTextNode(' '));
+      innerArea.appendChild(divGameArea);
+      divGameArea.id = "Y"+colInside+"X"+rowInside;
+      divGameArea.className = contentFromMap[colInside][rowInside][0];
+      divGameArea.appendChild(document.createElement("div"));
+    }    
+  }
+  setSkin();
+  gameOver = false;
+}
+// document.addEventListener("keydown", ()=>function keyChks() {});
+
+function setSkin()
+{
+  for (let colInside = 0; colInside<16; colInside++)
+  {
+    for (let rowInside = 0; rowInside<19; rowInside++)
+    {  
+      const icon=document.getElementById("Y"+colInside+"X"+rowInside);
+      //    console.log(icon.className);
+      if (icon.className=="W")
+      {
+        buildWall(icon);
+      }
+      else if (icon.className=="P")
+      {
+        buildPlayer(icon);
+      }
+      else if (icon.className=="B")
+      {
+        buildBall(icon);
+      } 
+      else if (icon.className=="G")
+      {
+        buildTiles(icon);
+      }
+    }
+  }
+}
+
+function buildWall(icon)
+{
+  var allWall = icon.appendChild(document.createElement("img"));
+  allWall.setAttribute("src", "/Image/wall.png");
+  allWall.setAttribute("alt","Wall");
+  allWall.setAttribute("display", "flex");
+  allWall.setAttribute("width", "100%");
+  allWall.setAttribute("height", "100%");
+  allWall.setAttribute("float", "left");
+  allWall.setAttribute("top", "0");
+}
+
+function buildPlayer(icon)
+{
+  var allWall = icon.appendChild(document.createElement("img"));
+  allWall.setAttribute("src", "/Image/FishLightGreen.png");
+  allWall.setAttribute("alt","Wall");
+  allWall.setAttribute("display", "flex");
+  allWall.setAttribute("width", "100%");
+  allWall.setAttribute("height", "100%");
+  allWall.setAttribute("float", "left");
+  allWall.setAttribute("top", "0");
+}
+
+function buildTiles(icon)
+{
+  var allWall = icon.appendChild(document.createElement("img"));
+  allWall.setAttribute("src", "/Image/G-Area.svg");
+  allWall.setAttribute("alt","Wall");
+  allWall.setAttribute("display", "flex");
+  allWall.setAttribute("width", "100%");
+  allWall.setAttribute("height", "100%");
+  allWall.setAttribute("float", "left");
+  allWall.setAttribute("top", "0");
+}
+
+function buildBall(icon)
+{
+  var allWall = icon.appendChild(document.createElement("img"));
+  allWall.setAttribute("src", "/Image/2-2-grinning-face-emoji-png.png");
+  allWall.setAttribute("alt","Wall");
+  allWall.setAttribute("display", "flex");
+  allWall.setAttribute("width", "100%");
+  allWall.setAttribute("height", "100%");
+  allWall.setAttribute("float", "left");
+  allWall.setAttribute("top", "0");
+}
+
+var yNextPos = 0;
+var xNextPos = 0;
+var ChangePos = 0;
+var userDirection = "";
+var yNowPos = 0;
+var xNowPos = 0;
+
+document.getElementById("sokoBanBody").addEventListener("keypress", function(event)
+{
+  document.getElementById("msgArea").innerHTML="No Arrow Keys detected!!!";
+})
+
+var IsArrowKey = false;   
+document.getElementById("sokoBanBody").addEventListener("keydown", function(event)
+{
+  event.preventDefault();
+  switch (event.key){
+    case "ArrowLeft":
+    case "ArrowDown":    
+    case "ArrowUp":    
+    case "ArrowRight":
+      IsArrowKey = true;   
+      break;
+    default:
+      document.getElementById("msgArea").innerHTML="No Arrow Keys detected!!!";
+      break;
+  }
+})
+document.getElementById("sokoBanBody").addEventListener("keyup", function(event) 
+{
+  event.preventDefault();
+  if (IsArrowKey)
+  {
+  if (gameStart == false)
+  {
+    xNowPos = 11;
+    yNowPos = 11;
+    yNextPos = yNowPos;
+    xNextPos = xNowPos;
+    gameStart = true;
+    gameOver = false;
+  }
+  else
+  {
+    yNowPos = yNextPos;
+    xNowPos = xNextPos;
+  }
+  console.log("Now:"+xNowPos+yNowPos+"Next:"+xNextPos+yNextPos);  
+  const key = event.key;
+  // Initail Next Position = current position
+
+  var currentPos=document.getElementById("Y"+yNowPos+"X"+xNowPos);
+  switch (event.key) 
+  {
+    case "ArrowLeft":
+      userDirection = "L";
+      ChangePos=checkCanMove(yNowPos, xNowPos, userDirection);
+      xNextPos=xNextPos+ChangePos;
+    break;
+    case "ArrowRight":
+      userDirection = "R";
+      ChangePos=checkCanMove(yNowPos, xNowPos, userDirection);
+      xNextPos=xNextPos+ChangePos;
+    break;
+    case "ArrowUp":
+      userDirection = "U";
+      ChangePos=checkCanMove(yNowPos,xNowPos, userDirection);
+      yNextPos=yNextPos+ChangePos;
+    break;
+    case "ArrowDown":
+      userDirection = "D";
+      ChangePos=checkCanMove(yNowPos,xNowPos, userDirection);
+      yNextPos=yNextPos+ChangePos;
+    break;
+    default:
+    break;
+  }
+    gameStart = true;
+  if (ChangePos==0)
+  {
+      xNowPos=xNextPos;
+      yNowPos=yNextPos;
+  }
+  else
+  if (ChangePos==1 || ChangePos==-1)
+  {
+//    console.log(currentPos.className);
+      currentPos.className=" ";
+      currentPos.removeChild(currentPos.lastElementChild);
+      var nextPos=document.getElementById("Y"+yNextPos+"X"+xNextPos);
+      nextPos.className="P";
+      buildPlayer(nextPos);
+  } else
+  if (ChangePos==2 || changePos == -2)
+  {
+//      console.log(currentPos.className+" "+yNextPos+" "+xNextPos);
+      currentPos.className=" ";
+      currentPos.removeChild(currentPos.lastElementChild);
+      nextPos=document.getElementById("Y"+yNextPos+"X"+xNextPos);
+      nextPos.className="P";
+      buildPlayer(nextPos);
+  }
+  console.log("Now:"+xNowPos+yNowPos+"Next:"+xNextPos+yNextPos);  
+  } // End of funcation
+} //END only for ArrowKey
+);
+
+  
+function checkCanMove(y, x, nextDir){
+  if (nextDir == "L")  // Left 
+  {
+    var nextClass=document.querySelector("#Y"+y+"X"+(x-1));
+  }
+  else if (nextDir == "U") // Up
+  {
+    var nextClass=document.querySelector("#Y"+(y-1)+"X"+x);
+  }
+  else if (nextDir == "R") // Right
+  {
+    var nextClass=document.getElementById("Y"+y+"X"+(x+1));
+
+  }
+  else // Dowon
+  {
+    var nextClass=document.getElementById("Y"+(y+1)+"X"+x);
+  }
+  let returnNum = 0;
+  switch(nextClass.className)
+  {
+    case "W":
+      returnNum = 0; //All remain, no movement
+        return returnNum;
+        break;
+    case "B":
+      checkWallInfront(y, x);
+      break;
+    case " ":
+      returnNum = getMoving(y, x, nextDir );    
+      console.log(returnNum);
+      return returnNum;
+      break;
+    default:
+              // will change to 99 to exit game;
+       return 0;
+       break;
+  }
+}
+          
+function getMoving(y, x, nextDir)  
+{
+  if (nextDir === "L" || nextDir === "U")
+  {
+    return -1;
+  }
+  else if (nextDir === "R"|| nextDir === "D")
+  {
+    return 1;
+  }
+}        
 
   /* 
-  This is to find out P-User position. However this may not neccessary because
+          This is to find out P-User position. However this may not neccessary because
   assuming a fix position for user is always begin at Y11X11
-  */
 
-  /*
+
   for (let colInside = 0; colInside<16; colInside++)
     {
       for (let rowInside = 0; rowInside<19; rowInside++)
@@ -60,27 +296,3 @@ function keyIsRelease() {
     }
     console.log(document.getElementById(posIs).innerHTML);
   */
-  /*    
-  // Get all the span elements and loop through them
-  divGameArea.querySelectorAll("div").forEach(function(element){
-    // Check the textContent of the element for a match
-    if(element.textContent === "P"){
-      // Find the nearest ancestor div
-      let closest = element.closest("div")
-      // ...then do whatever you want with it
-      console.log("The " + closest.nodeName + " has an id of: " + closest.id);
-    }
-  });
-
-
-// Test page to create 10 Div
-const forDiv = document.createElement("div");
-
-// Add ID to DIV element:
-for (let count=0; count<10; count++){
-        document.getElementById("myDIV").appendChild(forDiv);
-        forDiv.setAttribute('id', count);
-        forDiv.id = "R"+count;    
-        console.log(forDiv.id);
-}
-*/
